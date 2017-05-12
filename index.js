646f7fdfd9e32f2502d149919eb465937108d6c1
DanGrund <hello@dangrund.com>
2017-05-12 11:42:56 -0600

    at CRD, missing U

--- index.js
+++ index.js
@@ -1,30 +1,68 @@
-var submitButton = document.querySelector('.submit-wish')
-var wishInput = document.querySelector('.wish-input')
-var wishList = document.querySelector('.wish-list')
+const submitButton = document.querySelector('.submit-wish')
+const wishInput = document.querySelector('.wish-input')
+const wishList = document.querySelector('.wish-list')
+let wishArray = []
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  loadPage()
+});
 
 submitButton.addEventListener('click', (e) => {
   e.preventDefault()
-  appendItem()
+  const newWish = wishInput.value
+  storeWish(newWish)
+  renderWishes(wishArray)
   clearInput()
 })
 
 wishList.addEventListener('click', (e) => {
   if (e.target.className === "remove-button"){
-    deleteCard(e)
+    const wishText = (e.target.parentNode.firstChild.data)
+    deleteCard(wishText)
+    renderWishes(wishArray)
   }
 })
 
-var appendItem = function() {
-  var inputValue = wishInput.value
-  var listItem = document.createElement('p')
-  listItem.innerHTML = inputValue + `<button class="remove-button">remove</button>`
+const appendItem = (newWish) => {
+  const inputValue = wishInput.value
+  const listItem = document.createElement('p')
+  listItem.setAttribute('contenteditable', 'true')
+  listItem.innerHTML = newWish + `<button class="remove-button">remove</button>`
   wishList.appendChild(listItem)
 }
 
-var clearInput = function() {
+const clearInput = () => {
   wishInput.value = ""
 }
 
-var deleteCard = function(e) {
-  ((e.target).parentNode).remove()
+const deleteCard = (text) => {
+  for (let i = 0; i < wishArray.length; i++) {
+      const wish = wishArray[i]
+      if (text == wish) {
+        wishArray.splice(i, 1)
+      }
+      localStorage.setItem("wishArray",JSON.stringify(wishArray))
+    }
+}
+
+const loadPage = () => {
+  const holdingValue = JSON.parse(localStorage.getItem("wishArray"));
+  if (holdingValue){
+    wishArray = holdingValue;
+    renderWishes(wishArray);
+  }
+}
+
+const storeWish = (wish) => {
+  wishArray.push(wish);
+  localStorage.setItem("wishArray", JSON.stringify(wishArray));
+}
+
+const renderWishes = (newArray) => {
+  if (newArray) wishArray = newArray;
+  let wishList = document.querySelector('.wish-list');
+  while (wishList.firstChild) wishList.removeChild(wishList.firstChild);
+  for (let i = 0; i < wishArray.length; i++) {
+  appendItem(wishArray[i]);
+  }
 }
